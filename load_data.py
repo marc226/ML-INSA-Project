322d7a178e74cd0817dc51cc17abc8dd2ec16459
ManuelFount <manuel@fountapp.io>
2021-10-12 08:58:11 +0200

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,15 +1,19 @@
 import numpy as np
 import torch
 import torchvision
+import torch.optim as optim
+import torch.nn as nn
+from net import Net
+import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
-train_dir = './train_images'
+train_dir = './02big'
 test_dir = './test_images'
 
 transform = transforms.Compose(
-    [transforms.Grayscale(), 
-     transforms.ToTensor(), 
+    [transforms.Grayscale(),
+     transforms.ToTensor(),
      transforms.Normalize(mean=(0,),std=(1,))])
 
 train_data = torchvision.datasets.ImageFolder(train_dir, transform=transform)
@@ -27,11 +31,45 @@ train_new_idx, valid_idx = indices_train[split_tv:],indices_train[:split_tv]
 train_sampler = SubsetRandomSampler(train_new_idx)
 valid_sampler = SubsetRandomSampler(valid_idx)
 
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=train_sampler, num_workers=1)
-valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=valid_sampler, num_workers=1)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=1)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=train_sampler)
+valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=valid_sampler)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True)
+
 classes = ('noface','face')
+net = Net()
 
 # for epoch in range(1, n_epochs+1):
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+    print("epoch: " + str(epoch))
+    running_loss = 0.0
+    varEnum = enumerate(train_loader)
+
+    for i, data in enumerate(train_loader):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 200 == 199:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 200))
+            running_loss = 0.0
+
+print('Finished Training')
 # for data, target in train_loader:
+
+for i, data in enumerate(test_loader):
+
 
